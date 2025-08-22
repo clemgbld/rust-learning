ba187c91357aece777877b339c642464361b701a
cgombauld <clement.gombauld@cpexterne.org>
2025-08-22 10:19:36 +0200

    finished struct part

---
+++ projects/rectangles/src/main.rs
@@ -0,0 +1,26 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+
+impl Rectangle {
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
+}
+
+fn main() {
+    let rectangle = Rectangle::square(50);
+
+    println!("the area of the rectangle is {}", rectangle.area());
+}
